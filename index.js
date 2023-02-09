58dba319cd74fdd4f4d8ee2ada6d63569cc8d638
Tomás Belmar <tomasbelmar@MacBook-Air-de-Tomas.local>
2023-02-09 16:50:24 +0000

    lab done

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const panquecas = ({
+  title: "Panquecas de Aveia",
+  level:"Easy Peasy",
+  ingredients:"Colocar os ovos numa taça juntar a farinha de aveia e o iogurte natural e mexer",
+  dishType:"breakfast",
+  image: "https://anasousanutricionista.com/wp-content/uploads/2021/10/Panqueca-de-Aveia-1-1024x1024.jpg",
+  duration: 10,
+  creator:"Chef"
+});
+
 //Method 1 : Using Async Await
 
 const manageRecipes = async () => {
@@ -18,7 +28,19 @@ const manageRecipes = async () => {
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
 
+    await Recipe.create(panquecas)
+    const insert = await Recipe.insertMany(data)
+    insert.forEach(insertTitle => console.log(insertTitle.title));
+    const updatedRecipe = await Recipe.updateOne(
+      { duration: 220 },
+      { duration: 100 }
+    );
+    console.log("Recipe Updated");
+    const deleteRecipe = await Recipe.deleteOne({title: "Carrot Cake"});
+    console.log ("Recipe Delete")
+
     // Run your code here, after you have insured that the connection was made
+    dbConnection.disconnect();
   } catch (error) {
     console.log(error);
   }
